2c461a16603e1148a3330ff6b4f917688c1f4c64
Tyler Beringer <99tylerberinger@gmail.com>
2024-10-25 15:00:30 -0400

    Improved code

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # zero-day-calendar-calculator
-Get what date it is on a zero-day calendar from the 2023 calendar
+Get the date on a zero-day calendar with 13 months (28 days each). ZERO DAY is placed at the end of the year and LEAPYEAR DAY is placed on February_29
 # Getting Started
 Get this project set up locally
 ### Setting up
--- src/main.py
+++ src/main.py
@@ -1,19 +1,41 @@
 
 def main():
-    months = {"January": 31, "February": 28, "March": 31, "April": 30, "May": 31, "June": 30, "July": 31, "August": 31, "September": 30, "October": 31, "November": 30, "December": 31}
+
+    # variables
+    months = {
+        "January": 31,
+        "February": 29, # 2024 is a leap year
+        "March": 31,
+        "April": 30,
+        "May": 31,
+        "June": 30,
+        "July": 31,
+        "August": 31,
+        "September": 30,
+        "October": 31,
+        "November": 30,
+        "December": 31
+    }
+    
     dates = []
     for month in months:
         for i in range(months[month]):
             dates.append(f"{month}_{i+1}")
 
-    zeroday_dates = ["ZERODAY"]
+    zeroday_dates = []
     for i in range(13):
         for ii in range(28):
             zeroday_dates.append(f"Month{i+1}_{ii+1}")
+    zeroday_dates.append("ZERO DAY")
+    if months["February"] == 29:
+        zeroday_dates.insert(59, "LEAPYEAR DAY")
 
-    input_month = input("Enter the month: ")
-    input_day = input("Enter the day: ")
-    print(zeroday_dates[dates.index(f"{input_month}_{input_day}")])
+    # output
+    print("Month_Day ---> ???_???")
+    month = input("Enter month: ")
+    day = input("Enter day: ")
+    print(f"{month}_{day} ---> {zeroday_dates[dates.index(f"{month}_{day}")]}")
+
 
 if __name__ == "__main__":
     main()
